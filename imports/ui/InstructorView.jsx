248ee6e6936eb8b95f44b492cc4608fd99c16ae7
andrewfeikema <43078373+andrewfeikema@users.noreply.github.com>
2021-10-19 13:26:28 -0400

    separated instructor solution view

--- imports/ui/InstructorView.jsx
+++ imports/ui/InstructorView.jsx
@@ -1,37 +1,22 @@
-import { Meteor } from 'meteor/meteor';
-import React, { useState, Fragment, useEffect } from "react";
-import { getModules, Module } from "./Module.jsx";
-import { Feedback } from "./Feedback.jsx";
-import { useTracker } from 'meteor/react-meteor-data';
-import { LoginForm } from './LoginForm.jsx';
+import React, { Fragment } from "react";
+import { getModules } from "./Module.jsx";
+import { useTracker } from "meteor/react-meteor-data";
+import { Solution } from "./Solution.jsx";
 
 export const InstructorView = ({ user }) => {
   const [modules, setModules] = useTracker(() => {
     return getModules(user);
   });
 
-  const getStudentName = (id) => {
-      const student = Meteor.users.find({ _id: id }).fetch()[0];
-      return student.username;
-  }
-
-  const submitFeedback = () => {
-      alert("submitted")
-  }
-
   return (
     <div className="InstructorView">
-        <Fragment>
-            {getModules(user).map((module) => 
-                <Fragment key={module._id}>
-                    <Module module={module} title={getStudentName(module.user)}/>
-                    <form onSubmit={submitFeedback}>
-                        <input></input>
-                        <button>Submit</button>
-                    </form>
-                </Fragment>
-            )}
-        </Fragment>
+      <Fragment>
+        {getModules(user).map((module) => {
+          return (
+            <Solution module={module} key={module._id}></Solution>
+          );
+        })}
+      </Fragment>
     </div>
   );
 };
