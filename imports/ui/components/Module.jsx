24c70bf19760ec47b30868d2110876971febdbab
andrewfeikema <43078373+andrewfeikema@users.noreply.github.com>
2021-10-28 16:00:18 -0400

    static feedback

--- imports/ui/components/Module.jsx
+++ imports/ui/components/Module.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect, useContext, useRef } from 'react';
 import { useTracker } from 'meteor/react-meteor-data';
 import { ModulesCollection, RunsCollection, SessionsCollection, SnapshotsCollection } from '../../api/modules';
-import { CompilationRequestContext, SessionContext } from '../App';
+import { CompilationRequestContext } from '../App';
 
 import AceEditor from "react-ace";
 import "ace-builds/src-noconflict/mode-python";
@@ -29,9 +29,10 @@ export const getStudentsBySession = ({ session }) => {
   return SessionsCollection.findOne({ session }).users;
 }
 
-export const createSnapshot = ({ module, code, date }) => {
+export const createSnapshot = ({ module, code, output, date }) => {
   return SnapshotsCollection.insert({
     code,
+    output,
     createdAt: date,
     session: module.session,
     user: module.user,
@@ -97,7 +98,7 @@ export const Module = ({ module, title, onSelectionChange, readonly, region }) =
     const lastSnapshotDate = SnapshotsCollection.findOne({ user: module.user, session: module.session }, { sort: { createdAt: -1 }}).createdAt;
     if ((time.getTime() - lastSnapshotDate.getTime()) > MIN_SNAPSHOT_DELAY) {
       // add snapshot to snapshot collection
-      createSnapshot({ module, code: currentSnapshot, date: time });
+      createSnapshot({ module, code: currentSnapshot, output, date: time });
     }
   }
 
@@ -168,7 +169,6 @@ export const Module = ({ module, title, onSelectionChange, readonly, region }) =
             useSoftTabs: true
           }}
           highlightActiveLine={false}
-          onSelectionChange={onSelectionChange ? onSelectionChange : () => {}}
           height="400px"
           width="600px"
           onChange={onChange}
--- imports/ui/components/Solution.jsx
+++ imports/ui/components/Solution.jsx
@@ -1,40 +1,64 @@
 import { FeedbackCollection } from "../../api/modules";
-import React, { useState, Fragment, useContext, useEffect } from "react";
-import { createSnapshot, getSnapshotsByStudentSession, Module } from "./Module";
+import { ResultViewer } from "./Module";
+import React, { useState, useContext } from "react";
 import { SessionContext } from "../App";
 import { getSnapshotByStudentSessionDate } from "./Module";
-
+import AceEditor from "react-ace";
 export const Solution = ({ module }) => {
   const [feedback, setFeedback] = useState("");
-  const [selectedUser, setSelectedUser] = useState(null);
   const [selection, setSelection] = useState(null);
+
   const { session } = useContext(SessionContext);
+
+  const snap = getSnapshotByStudentSessionDate({
+    user: module.user,
+    session,
+    date: module.createdAt,
+  });
 
   const submitFeedback = () => {
     // check if snapshot of current code exists,
     // if not, create it and reference it in feedback collection
     let snapshotID;
-    
-    const snap = getSnapshotByStudentSessionDate({ user: module.user, session, date: module.createdAt });
-    if (!snap) {
-      snapshotID = createSnapshot({ module, code: module.code, date: module.createdAt });
-    } else {
-      snapshotID = snap._id;
-    }
 
-    FeedbackCollection.insert({ 
+    snapshotID = snap?._id;
+
+    FeedbackCollection.insert({
       body: feedback,
-      module: module._id, 
+      module: module._id,
       snapshot: snapshotID,
-      region: selection ? selection.getAllRanges() : [], 
+      region: selection ? selection.getAllRanges() : [],
     });
   };
 
   return (
-    <Fragment key={module._id}>
-      <Module module={module} title={module.user.username} onSelectionChange={(newRegion) => { setSelection(newRegion) }} readonly={false} region={[]} />
-      <input onInput={(e) => setFeedback(e.target.value)}/>
+    <div key={module._id}>
+      <div className="module-container">
+        <h3>{module.user.username}</h3>
+
+        <AceEditor
+          mode="python"
+          theme="github"
+          setOptions={{
+            useSoftTabs: true,
+          }}
+          highlightActiveLine={false}
+          onSelectionChange={(newRegion) => {
+            setSelection(newRegion);
+          }}
+          height="400px"
+          width="600px"
+          debounceChangePeriod={1000}
+          name={module._id}
+          readOnly={true}
+          editorProps={{ $blockScrolling: true }}
+          value={module.code}
+        />
+
+        <ResultViewer className="cool" module_id={module._id} />
+      </div>
+      <input onInput={(e) => setFeedback(e.target.value)} />
       <button onClick={submitFeedback}>Submit</button>
-    </Fragment>
+    </div>
   );
 };
