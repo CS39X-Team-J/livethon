5167287dc4f16d16be44d876b169fd79de80e295
Jacob-Brink <jacobpeterbrink@gmail.com>
2022-03-09 08:34:49 -0500

    Cleanup console.log

--- imports/ui/services/CodeSnapshot.js
+++ imports/ui/services/CodeSnapshot.js
@@ -1,7 +1,6 @@
 import { createRun } from "../../api/methods/createRun";
 
 export const execute = async (moduleID, code, request, createdAt) => {
-    console.log("marker")
     let output;
     try {
         const results = await request({ id: moduleID, code });
